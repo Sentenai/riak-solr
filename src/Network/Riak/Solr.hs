89b143b05ad4140464ed047da7c95e56c2a91355
Mitchell Rosen <mitchellwrosen@gmail.com>
2017-03-21 16:14:54 -0400

    Update to solr-query 0.4.0

--- src/Network/Riak/Solr.hs
+++ src/Network/Riak/Solr.hs
@@ -3,20 +3,23 @@ module Network.Riak.Solr
     search
     -- * Re-exports
   , module Solr.Query
+  , module Solr.Query.Lucene
   ) where
 
 import Data.ByteString.Lazy (ByteString, fromStrict)
+import Data.Maybe (catMaybes)
 import Data.Monoid (Endo(..))
 import Data.Semigroup
 import Data.Sequence ((|>))
 import Data.Text (Text)
+import Data.Text.Lazy.Builder (Builder)
 import Network.Riak.Connection (exchange)
 import Network.Riak.Protocol.SearchQueryRequest (SearchQueryRequest)
 import Network.Riak.Types (Connection)
 import Network.Riak.Types.Internal (Index, SearchResult)
 import Prelude.Compat
 import Solr.Query
-import Solr.Query.Initial
+import Solr.Query.Lucene
 
 import qualified Data.Text.Encoding as Text (encodeUtf8)
 import qualified Data.Text.Lazy as LText (Text)
@@ -25,33 +28,46 @@ import qualified Data.Text.Lazy.Encoding as LText (encodeUtf8)
 import qualified Network.Riak.Protocol.SearchQueryRequest as Req
 import qualified Network.Riak.Response as Resp (search)
 import qualified Text.ProtocolBuffers as Proto
+import qualified Solr.Query as Solr
+import qualified Solr.Query.Internal as Solr
+import qualified Solr.Query.Lucene as Solr
+import qualified Solr.Query.Lucene.Internal as Solr
 
 search
-  :: Connection -> Index -> [Param] -> [LocalParam 'QueryLocalParam] -> Query
-  -> IO SearchResult
+  :: Connection -> Index -> [Solr.Param] -> Solr.LocalParams Solr.LuceneQuery
+  -> Solr.LuceneQuery -> IO SearchResult
 search conn index params locals query = Resp.search <$> exchange conn request
   where
     request :: SearchQueryRequest
-    request = appEndo (foldMap f params <> foldMap g locals)
+    request = appEndo (foldMap (Endo . f) params <> g locals)
       (Proto.defaultValue
-        { Req.q = lt2lbs (Builder.toLazyText (compileQuery query))
+        { Req.q = b2lbs (Solr.coerceQuery query)
         , Req.index = index
         })
       where
-        f :: Param -> Endo SearchQueryRequest
+        f :: Solr.Param -> SearchQueryRequest -> SearchQueryRequest
         f = \case
-          ParamFl s       -> Endo (appendFl s)
-          ParamFq _ _     -> mempty -- I don't believe riak supports filter queries
-          ParamRows n     -> Endo (setRows n)
-          ParamSortAsc s  -> Endo (setSort (s <> " asc"))
-          ParamSortDesc s -> Endo (setSort (s <> " desc"))
-          ParamStart n    -> Endo (setStart n)
+          Solr.ParamFl s       -> appendFl s
+          -- I don't believe riak supports filter queries
+          Solr.ParamFq _ _     -> id
+          Solr.ParamRows n     -> setRows n
+          Solr.ParamSortAsc s  -> setSort (s <> " asc")
+          Solr.ParamSortDesc s -> setSort (s <> " desc")
+          Solr.ParamStart n    -> setStart n
 
-        g :: LocalParam 'QueryLocalParam -> Endo SearchQueryRequest
-        g = \case
-          LocalParamDf s  -> Endo (setDf s)
-          LocalParamOpAnd -> Endo (setOp "AND")
-          LocalParamOpOr  -> Endo (setOp "OR")
+        g :: Solr.LocalParams Solr.LuceneQuery -> Endo SearchQueryRequest
+        g Solr.LuceneParams{Solr.paramDf, Solr.paramQOp} =
+          mconcat (catMaybes
+            [ (Endo . setDf) <$> paramDf
+            , (\case
+                Solr.QOpAnd -> Endo (setOp "AND")
+                Solr.QOpOr  -> Endo (setOp "OR"))
+              <$> paramQOp
+            ])
+
+          -- LocalParamDf s  -> setDf s
+          -- LocalParamOpAnd -> setOp "AND"
+          -- LocalParamOpOr  -> setOp "OR"
 
 setDf :: Text -> SearchQueryRequest -> SearchQueryRequest
 setDf s q = q { Req.df = Just (t2lbs s) }
@@ -76,3 +92,6 @@ t2lbs = fromStrict . Text.encodeUtf8
 
 lt2lbs :: LText.Text -> ByteString
 lt2lbs = LText.encodeUtf8
+
+b2lbs :: Builder -> ByteString
+b2lbs = lt2lbs . Builder.toLazyText
