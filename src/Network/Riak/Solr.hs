4d019db630901b786cc4be717abd70a99f5e838f
Yu-ri Gordon <yuri.cho@gmail.com>
2021-05-27 13:24:32 -0400

    updating fields to fit proto lens

--- src/Network/Riak/Solr.hs
+++ src/Network/Riak/Solr.hs
@@ -1,75 +1,83 @@
 module Network.Riak.Solr
   ( -- * Search
-    search
-  , searchRequest
-    -- * Re-exports
-  , module Solr.Query
-  , module Solr.Query.Lucene
-  ) where
+    search,
+    searchRequest,
 
-import Data.ByteString.Lazy (ByteString, fromStrict)
+    -- * Re-exports
+    module Solr.Query,
+    module Solr.Query.Lucene,
+  )
+where
+
+import Data.ByteString.Lazy (ByteString, fromStrict, toStrict)
 import Data.List (intersperse)
 import Data.Maybe (catMaybes)
-import Data.Monoid (Endo(..))
+import qualified Data.Riak.Proto as Proto
 import Data.Semigroup
-import Data.Sequence ((|>))
 import Data.Text (Text)
-import Data.Text.Lazy.Builder (Builder)
-import Network.Riak.Connection (exchange)
-import Network.Riak.Protocol.SearchQueryRequest (SearchQueryRequest)
-import Network.Riak.Types (Connection)
-import Network.Riak.Types.Internal (Index, SearchResult)
-import Prelude.Compat
-import Solr.Query
-import Solr.Query.Lucene
-
 import qualified Data.Text.Encoding as Text (encodeUtf8)
 import qualified Data.Text.Lazy as LText (Text)
+import Data.Text.Lazy.Builder (Builder)
 import qualified Data.Text.Lazy.Builder as Builder (toLazyText)
 import qualified Data.Text.Lazy.Encoding as LText (encodeUtf8)
-import qualified Network.Riak.Protocol.SearchQueryRequest as Req
+import Network.Riak.Connection (exchange)
+import Network.Riak.Lens
 import qualified Network.Riak.Response as Resp (search)
-import qualified Text.ProtocolBuffers as Proto
+import Network.Riak.Types.Internal hiding (MessageTag (..))
+import Prelude.Compat
+import Solr.Query
 import qualified Solr.Query as Solr
 import qualified Solr.Query.Internal as Solr
+import Solr.Query.Lucene
 import qualified Solr.Query.Lucene as Solr
 import qualified Solr.Query.Lucene.Internal as Solr
 
-search
-  :: Connection -> Index -> [Solr.Param] -> Solr.LocalParams Solr.LuceneQuery
-  -> Solr.LuceneQuery -> IO SearchResult
+search ::
+  Connection ->
+  Index ->
+  [Solr.Param] ->
+  Solr.LocalParams Solr.LuceneQuery ->
+  Solr.LuceneQuery ->
+  IO SearchResult
 search conn index params locals query =
   Resp.search <$> exchange conn (searchRequest index params locals query)
 
-searchRequest
-  :: Index -> [Solr.Param] -> Solr.LocalParams Solr.LuceneQuery
-  -> Solr.LuceneQuery -> SearchQueryRequest
+searchRequest ::
+  Index ->
+  [Solr.Param] ->
+  Solr.LocalParams Solr.LuceneQuery ->
+  Solr.LuceneQuery ->
+  Proto.RpbSearchQueryReq
 searchRequest index params locals query =
-  appEndo (foldMap (Endo . f) params <> g locals)
-    (Proto.defaultValue
-      { Req.q = b2lbs (Solr.coerceQuery query)
-      , Req.index = index
-      })
+  appEndo
+    (foldMap (Endo . f) params <> g locals)
+    ( Proto.defMessage
+        & Proto.q .~ (toStrict $ b2lbs (Solr.coerceQuery query))
+        & Proto.index .~ index
+    )
   where
-    f :: Solr.Param -> SearchQueryRequest -> SearchQueryRequest
+    f :: Solr.Param -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
     f = \case
-      Solr.ParamFl s       -> appendFl s
+      Solr.ParamFl s -> appendFl s
       -- I don't believe riak supports filter queries
-      Solr.ParamFq _ _     -> id
-      Solr.ParamRows n     -> setRows n
-      Solr.ParamSort []    -> id
-      Solr.ParamSort ss    -> setSort (toSortString ss)
-      Solr.ParamStart n    -> setStart n
+      Solr.ParamFq _ _ -> id
+      Solr.ParamRows n -> setRows n
+      Solr.ParamSort [] -> id
+      Solr.ParamSort ss -> setSort (toSortString ss)
+      Solr.ParamStart n -> setStart n
 
-    g :: Solr.LocalParams Solr.LuceneQuery -> Endo SearchQueryRequest
-    g Solr.LuceneParams{Solr.paramDf, Solr.paramQOp} =
-      mconcat (catMaybes
-        [ (Endo . setDf) <$> paramDf
-        , (\case
-            Solr.QOpAnd -> Endo (setOp "AND")
-            Solr.QOpOr  -> Endo (setOp "OR"))
-          <$> paramQOp
-        ])
+    g :: Solr.LocalParams Solr.LuceneQuery -> Endo Proto.RpbSearchQueryReq
+    g Solr.LuceneParams {Solr.paramDf, Solr.paramQOp} =
+      mconcat
+        ( catMaybes
+            [ (Endo . setDf) <$> paramDf,
+              ( \case
+                  Solr.QOpAnd -> Endo (setOp "AND")
+                  Solr.QOpOr -> Endo (setOp "OR")
+              )
+                <$> paramQOp
+            ]
+        )
 
 toSortString :: [(Text, SortWay)] -> Text
 toSortString =
@@ -79,23 +87,25 @@ toSortString =
     f (s, Asc) = s <> " asc"
     f (s, Desc) = s <> " desc"
 
-setDf :: Text -> SearchQueryRequest -> SearchQueryRequest
-setDf s q = q { Req.df = Just (t2lbs s) }
+setDf :: Text -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+setDf s q = q & Proto.df .~ (toStrict $ t2lbs s)
 
-appendFl :: Text -> SearchQueryRequest -> SearchQueryRequest
-appendFl s q = q { Req.fl = Req.fl q |> t2lbs s }
+appendFl :: Text -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+appendFl s q = q & Proto.fl .~ ((q ^. Proto.fl) ++ [toStrict . t2lbs $ s])
 
-setOp :: ByteString -> SearchQueryRequest -> SearchQueryRequest
-setOp o q = q { Req.op = Just o }
+--q { Req.fl = Req.fl q |> t2lbs s }
 
-setRows :: Int -> SearchQueryRequest -> SearchQueryRequest
-setRows n q = q { Req.rows = Just (fromIntegral n) }
+setOp :: ByteString -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+setOp o q = q & Proto.op .~ (toStrict o)
 
-setSort :: Text -> SearchQueryRequest -> SearchQueryRequest
-setSort s q = q { Req.sort = Just (t2lbs s) }
+setRows :: Int -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+setRows n q = q & Proto.rows .~ (fromIntegral n)
 
-setStart :: Int -> SearchQueryRequest -> SearchQueryRequest
-setStart n q = q { Req.start = Just (fromIntegral n) }
+setSort :: Text -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+setSort s q = q & Proto.sort .~ (toStrict (t2lbs s))
+
+setStart :: Int -> Proto.RpbSearchQueryReq -> Proto.RpbSearchQueryReq
+setStart n q = q & Proto.start .~ (fromIntegral n)
 
 t2lbs :: Text -> ByteString
 t2lbs = fromStrict . Text.encodeUtf8
